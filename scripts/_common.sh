3749a27cb23fdafe5da2ab1cd1b26e2a8a933ab6
tituspijean <tituspijean@outlook.com>
2021-05-25 10:22:08 +0200

    Support other arm architectures

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,10 +5,9 @@
 #=================================================
 
 version="3.1.1.4954"
-architecture=$(dpkg --print-architecture)
-if [ $architecture = "amd64" ]; then
-  architecture="x64"
-fi
+
+# Supported architectures
+supported_architectures=("arm" "arm64" "x64")
 
 # dependencies used by the app
 pkg_dependencies="curl mediainfo sqlite3"
@@ -16,6 +15,19 @@ pkg_dependencies="curl mediainfo sqlite3"
 #=================================================
 # PERSONAL HELPERS
 #=================================================
+
+get_architecture() {
+	architecture=$(dpkg --print-architecture)
+	if [ $architecture = "amd64" ]; then
+		architecture="x64"
+	elif [[ $architecture = arm* ]] && [[ $(getconf LONG_BIT) = 32 ]]; then
+		architecture="arm"
+	elif [[ $architecture = arm* ]] && [[ $(getconf LONG_BIT) = 64 ]]; then
+		architecture="arm64"
+	elif [[ $(echo ${supported_architectures[@]} | grep -ow "$architecture" | wc -w) = 0 ]]; then
+		ynh_die --message="Unsupported architecture $architecture"
+	fi
+}
 
 #=================================================
 # EXPERIMENTAL HELPERS
